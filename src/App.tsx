6581792c40fb5f51d6840b115fa7134b058f945d
limjeahyuk <lim0202jh@gmail.com>
2026-05-08 15:24:17 +0900

    feat: reset css

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <h1 style={{ backgroundColor: "#55f3ac" }}>h1 test</h1>
+
+      <h2>h2 test</h2>
+
+      <h3>h3 test</h3>
+
+      <h4>h4 test</h4>
+
+      <p>p test</p>
+      <span> span test</span>
+      <a href="#">a test</a>
+      <br />
+      <input type="text" value="input test" />
+      <textarea value="textarea test" />
+    </div>
+  );
+}
+
+export default App;
